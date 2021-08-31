360396f4ccd552d407728a6b2f522f79e37bb6a4
AnNguyen <an.nguyen.wa@gmail.com>
2021-08-31 04:22:46 -0700

    Section 19

--- Section 16 JavaScript Decision Making/script.js
+++ Section 16 JavaScript Decision Making/script.js
@@ -1,2 +1,33 @@
-console.log("Hello");
-// print in the console, not main page
+// const password = prompt("Enter a password");
+
+// if (password.length >= 6) {
+//     if (password.indexOf(" ") === -1) {
+//         console.log("valid password!");
+//     } else {
+//         console.log("no space please");
+//     }
+// } else {
+//     console.log("too short");
+// }
+
+// if (password) {
+//     console.log("Truthy");
+// } else {
+//     console.log("Falsy");
+// }
+
+let maximum = parseInt(prompt("Enter a max num"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a max num"));
+}
+let target = Math.floor(Math.random() * maximum) + 1;
+let guess = parseInt(prompt("Enter a guess"));
+while (guess !== target) {
+    if (guess > target) {
+        guess = parseInt(prompt("too high"));
+    } else {
+        guess = parseInt(prompt("too low"));
+    }
+}
+
+console.log(target);
