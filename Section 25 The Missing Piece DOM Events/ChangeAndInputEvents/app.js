a261d58c9ec0c251927fdba3df59e510273b324d
AnNguyen <an.nguyen.wa@gmail.com>
2021-09-03 01:13:24 -0700

    Section 25

--- Section 25 The Missing Piece DOM Events/ChangeAndInputEvents/app.js
+++ Section 25 The Missing Piece DOM Events/ChangeAndInputEvents/app.js
@@ -5,6 +5,10 @@ const h1 = document.querySelector('h1');
 //     console.log("CASKDJASKJHD")
 // })
 
+// option 'change' to check if the input is changed
+// enter, change values and leave the input
+
+// change right away for each character added/removed
 input.addEventListener('input', function (e) {
     h1.innerText = input.value;
 })
--- Section 25 The Missing Piece DOM Events/FormEvents/app.js
+++ Section 25 The Missing Piece DOM Events/FormEvents/app.js
@@ -3,13 +3,16 @@
 const tweetForm = document.querySelector('#tweetForm');
 const tweetsContainer = document.querySelector('#tweets');
 tweetForm.addEventListener('submit', function (e) {
+
+    //to prevent the action, go to /dog
+    // need to be 1st, not last
     e.preventDefault();
 
     // const usernameInput = document.querySelectorAll('input')[0];
     // const tweetInput = document.querySelectorAll('input')[1];
     const usernameInput = tweetForm.elements.username;
     const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
+    addTweet(usernameInput.value, tweetInput.value);
     usernameInput.value = '';
     tweetInput.value = '';
 });
@@ -17,8 +20,8 @@ tweetForm.addEventListener('submit', function (e) {
 const addTweet = (username, tweet) => {
     const newTweet = document.createElement('li');
     const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
+    bTag.append(username) //make username bold
+    newTweet.append(bTag); //add username to tweet
     newTweet.append(`- ${tweet}`)
     tweetsContainer.append(newTweet);
 }
