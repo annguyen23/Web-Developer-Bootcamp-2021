779dcb2c69a71422cdc89730c367638c11ba4cda
AnNguyen <an.nguyen.wa@gmail.com>
2021-09-05 00:58:55 -0700

    Section 29

--- Section 29 Prototyes, Classes, OOP/03_Factory_Functions/app.js
+++ Section 29 Prototyes, Classes, OOP/03_Factory_Functions/app.js
@@ -5,11 +5,11 @@ function makeColor(r, g, b) {
 	color.r = r;
 	color.g = g;
 	color.b = b;
-	color.rgb = function() {
+	color.rgb = function () {
 		const { r, g, b } = this;
 		return `rgb(${r}, ${g}, ${b})`;
 	};
-	color.hex = function() {
+	color.hex = function () {
 		const { r, g, b } = this;
 		return (
 			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
@@ -17,6 +17,8 @@ function makeColor(r, g, b) {
 	};
 	return color;
 }
+//example: h1.style.color = makeColor(234,56, 45).hex();
+
 
 const firstColor = makeColor(35, 255, 150);
 firstColor.hex(); //firstColor.hex();
@@ -25,3 +27,11 @@ firstColor.rgb(); //"rgb(35, 255, 150)"
 const black = makeColor(0, 0, 0);
 black.rgb(); //"rgb(0, 0, 0)"
 black.hex(); //"#0000s00"
+
+
+const h1 = document.querySelector("h1");
+let r = Math.round(Math.random() * 255);
+let g = Math.round(Math.random() * 255);
+let b = Math.round(Math.random() * 255);
+h1.style.color = makeColor(r, g, b).hex();
+
