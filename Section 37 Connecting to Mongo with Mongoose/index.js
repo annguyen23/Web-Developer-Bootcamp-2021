1a1750363031bcf27debeb6197321b81ac417cbb
AnNguyen <an.nguyen.wa@gmail.com>
2021-09-08 01:57:00 -0700

    Section 37

--- Section 37 Connecting to Mongo with Mongoose/index.js
+++ Section 37 Connecting to Mongo with Mongoose/index.js
@@ -1,3 +1,5 @@
+// node > .load index.js
+// on another ternimal: mongo > use movieApp
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -14,11 +16,26 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
 // db.once('open', function () {
 //     console.log('CONNECTION OPEN')
 // })
-const movieSchema = new Schema({
-    title: String, // String is shorthand for {type: String}
+
+const movieSchema = new mongoose.Schema({
+    title: String,
     year: Number,
     score: Number,
     rating: String
 });
 
-const Blog = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model('Movie', movieSchema);
+
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+    .then(data => {
+        console.log("It worked");
+        console.log(data);
+    })
+
