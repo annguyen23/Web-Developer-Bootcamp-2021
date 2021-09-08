93a4963b1c72963bed47ebaa14bf3fd42d7c9f44
AnNguyen <an.nguyen.wa@gmail.com>
2021-09-08 04:07:03 -0700

    Section 37

--- Section 37 Connecting to Mongo with Mongoose/product.js
+++ Section 37 Connecting to Mongo with Mongoose/product.js
@@ -12,16 +12,22 @@ const productSchema = new mongoose.Schema({
     name: { // allow to add more information
         type: String,
         require: true,// name is require for each product
+        maxLength: 20
     },
     price: {
         type: Number,
         require: true,// name is require for each product
-    }
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: [String] // an array of String
 });
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Mountain Bike', price: 999 })
+const bike = new Product({ name: 'Mountain Bike', price: 999, categories: ['cycling', 'Safety', 123] })
 bike.save()
     .then(data => {
         console.log('It Worked');
