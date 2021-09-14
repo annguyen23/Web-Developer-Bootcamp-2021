f89984b811a9da486bb5b09e252c118c9fb5d38b
AnNguyen <an.nguyen.wa@gmail.com>
2021-09-14 04:36:52 -0700

    Section 46

--- Section 45 Mongo Relationships With Express/index.js
+++ Section 45 Mongo Relationships With Express/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
 const Product = require('./models/product');
+const Farm = require('./models/farm');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -19,6 +20,55 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", 'ejs');
 app.use(express.urlencoded({ extended: true })) // tell express to use middleware
 app.use(methodOverride('_method'));
+
+// FARM ROUTES
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', { farms })
+})
+
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+
+// '/farms/:id' need to be after '/farms/new'
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    res.render('farms/show', { farm })
+})
+
+app.delete('/farms/:id', async (req, res) => {
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+
+    res.redirect('/farms');
+})
+
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    res.redirect('/farms')
+})
+
+// show website create new products in a Farm
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`)
+})
+
+// PRODUCT ROUTES
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
@@ -73,9 +123,11 @@ app.get('/products/:id', async (req, res) => {
     if (!product) {
         res.send('not existed product');
     } else {
+        const farm = await Farm.findById(product.farm._id);
+        // console.log(farm.name);
         console.log(product);
         // res.send('details page');
-        res.render('products/show', { product })
+        res.render('products/show', { product, farm })
     }
 });
 
---
+++ Section 45 Mongo Relationships With Express/models/farm.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Product = require('./product');
+const { Schema } = mongoose;
+
+const farmSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Farm must have a name!"]
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, "Email required!"]
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Product"
+        }
+    ]
+})
+
+// farmSchema.pre('findOneAndDelete', async function (data) {
+//     console.log('Pre middle ware');
+//     console.log(data)
+// })
+
+farmSchema.post('findOneAndDelete', async function (farm) {
+    if (farm.products.length) {
+        const res = Product.deleteMany({ _id: { $in: farm.products } })
+        console.log(res)
+    }
+    // console.log('Post middle ware');
+    // console.log(data)
+})
+
+const Farm = mongoose.model('Farm', farmSchema);
+
+module.exports = Farm;
