08af1ab38524db5538f17329903dc4947d565858
AnNguyen <an.nguyen.wa@gmail.com>
2021-09-15 18:20:30 -0700

    Section 49

--- Section 49 YelpCamp Restructuring And Flash/app.js
+++ Section 49 YelpCamp Restructuring And Flash/app.js
@@ -14,6 +14,7 @@ app.use(methodOverride("_method")); // to use it, add '_method=<PUT,DELETE>'
 const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public'))); // to access files from folder public
 ///////////////////////////////////////////////////////////////////////
 
 
@@ -24,7 +25,6 @@ app.engine('ejs', ejsMate)
 
 //////////////////////////// joi and validation///////////////////////////////////
 const Joi = require("joi");
-const { campgroundSchema, reviewSchema } = require("./schemas.js");
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -54,7 +54,8 @@ const Review = require('./models/review');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     // useCreateIndex: true, useCreateIndex is always true, and it is no longer supported
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -70,121 +71,23 @@ const ExpressError = require("./utils/ExpressError");
 const campground = require('./models/campground');
 ///////////////////////////////////////////////////////////////////////
 
+const campgrounds = require('./routes/campground');
+const reviews = require('./routes/reviews');
 
-// home page at http://localhost:3000/
-app.get('/', (req, res) => {
-    res.render('home'); // open views/home.ejs
-});
-
-// app.get('/makecampground', async (req, res) => {
-//     const camp = new Campground({
-//         title: 'First Camp',
-//         price: 299,
-//         description: "No Description",
-//         location: "In the middle of nowhere"
-//     });
-//     await camp.save();
-//     console.log(camp);
-//     res.send(camp); // open views/home.ejs
-// })
-
-// main page that show all camgrounds
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds }); // open views/camgrounds/index.ejs
-}));
-
-// Show specific campground
-app.get('/campgrounds/new', catchAsync((req, res) => {
-    const campground = new Campground({});
-    res.render('campgrounds/new'); // open views/camgrounds/index.ejs
-}));
-
-// create new campground from localhost:3000/campgrounds/new
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
-    // if nothing is entered in the form
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-
-    // const camgroundSchema = Joi.object({
-    //     campground: Joi.object({
-    //         title: Joi.string().required(),
-    //         price: Joi.number().required().min(0),
-    //         image: Joi.string().required(),
-    //         location: Joi.string().required(),
-    //         description: Joi.string().required()
-    //     }).required()
-    // })
-    // const { error } = campgroundSchema.validate(req.body);
-    // if (error) {
-    //     const msg = error.details.map(el => el.message).join(',');
-    //     throw new ExpressError(msg, 400);
-    // }
-    // to get data from req.body, need app.use(express.urlencoded({extended: true}));
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`); // go to /campgrounds/:id
-}));
-
-// Show specific campground
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    console.log(campground);
-    res.render('campgrounds/show', { campground }); // open views/camgrounds/index.ejs
-}));
-
-// Show specific campground to edit
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground }); // open views/camgrounds/edit.ejs
-}));
-
-// update specific campground from /campgrounds/:id/edit
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    // second option
-    // const { title, location, image, price, description } = req.body.campground;
-    // const campground = await Campground.findByIdAndUpdate(id, { title: `${title}`, location: `${location}` });
-
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`); // go to /campgrounds/:id
-}));
-
-// delete specific campground from /campgrounds/:id
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds"); // go to /campgrounds
-}));
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    console.log(campground);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    const review = await Review.findByIdAndDelete(reviewId);
-    // res.send("Delete me")
-    res.redirect(`/campgrounds/${id}`)
-}))
+app.use("/campgrounds", campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 // all for all request
 // * for all paths
-// app.all('*', (req, res, next) => {
-//     next(new ExpressError('Page Not Found', 404))
-// })
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
 
-// app.use((err, req, res, next) => {
-//     const { statusCode = 500 } = err;
-//     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-//     res.status(statusCode).render('error', { err })
-// })
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 
 app.listen(3000, () => {
     console.log("Listen to port 3000");
--- Section 49 YelpCamp Restructuring And Flash/schemas.js
+++ Section 49 YelpCamp Restructuring And Flash/schemas.js
@@ -1,16 +1,15 @@
 const Joi = require("joi");
 
-const validateCampground = (req, res, next) => {
-    const camgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
-}
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+})
+
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
