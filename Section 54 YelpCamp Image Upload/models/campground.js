65035c55a8a9beb5838e716f71027d5783e2d442
AnNguyen <an.nguyen.wa@gmail.com>
2021-09-19 02:49:13 -0700

    Section 54

--- Section 54 YelpCamp Image Upload/models/campground.js
+++ Section 54 YelpCamp Image Upload/models/campground.js
@@ -3,12 +3,20 @@ const Review = require("./review");
 const User = require("./user");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampGroundSchema = new Schema({
     title: String,
-    images: [{
-        url: String,
-        filename: String
-    }],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -34,6 +42,7 @@ CampGroundSchema.post('findOneAndDelete', async function (campground) {
         })
     }
 })
+
 
 // export the CampGroundSchema under name Campground
 module.exports = mongoose.model('Campground', CampGroundSchema);
